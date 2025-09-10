ad54f130b44a8705d60ac9b04553c4ab1bc1fd2e
RizobekS <rizobeksaibov@gmail.com>
2025-09-10 10:17:19 +0500

    trying to fix 404 error in prod server

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "widget_tweaks",
+    'whitenoise.runserver_nostatic',
 
     # local apps
     "apps.common",
