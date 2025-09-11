117d4bfe6dbe2068eb349ede56e1e26105decff7
RizobekS <rizobeksaibov@gmail.com>
2025-09-11 15:24:27 +0500

    deleted the auth function and middleware from project

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "config.middleware.LoginRequiredMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
